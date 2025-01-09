0a4a6627b8b2ddb57a2c31fffaa29079a1485ae4
hendisantika <hendisantika@yahoo.co.id>
2025-01-09 07:34:14 +0700

    Add Location Model🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/weather_service/Weather.java
@@ -0,0 +1,53 @@
+package id.my.hendisantika.weather_service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : weather-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/01/25
+ * Time: 07.33
+ * To change this template use File | Settings | File Templates.
+ */
+public class Weather {
+  public static class Location {
+    private String code;
+    private String name;
+    private String timezone;
+    private Coordinates coordinates;
+
+    // Getters and setters
+    public String getCode() {
+      return code;
+    }
+
+    public void setCode(String code) {
+      this.code = code;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+
+    public String getTimezone() {
+      return timezone;
+    }
+
+    public void setTimezone(String timezone) {
+      this.timezone = timezone;
+    }
+
+    public Coordinates getCoordinates() {
+      return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+      this.coordinates = coordinates;
+    }
+  }
+}
