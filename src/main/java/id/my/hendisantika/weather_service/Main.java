7de643882f004e76cb39631979eb2df47e60b601
hendisantika <hendisantika@yahoo.co.id>
2025-01-09 07:39:43 +0700

    Add start in Main App🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/weather_service/Main.java
+++ src/main/java/id/my/hendisantika/weather_service/Main.java
@@ -1,6 +1,11 @@
 package id.my.hendisantika.weather_service;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.ext.web.Router;
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.AsyncHttpClientConfig;
+import org.asynchttpclient.Dsl;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,6 +19,30 @@ import io.vertx.core.AbstractVerticle;
  */
 public class Main {
   public static class MyVerticle extends AbstractVerticle {
+    private AsyncHttpClient asyncHttpClient;
 
+    @Override
+    public void start(Promise<Void> startPromise) {
+      Router router = Router.router(vertx);
+
+      router.get("/").handler(this::handleGetSummaryResource);
+      router.get("/hour").handler(this::handleGetHourResource);
+
+      // Initialize AsyncHttpClient
+      AsyncHttpClientConfig config = Dsl.config().build();
+      asyncHttpClient = Dsl.asyncHttpClient(config);
+
+      vertx.createHttpServer()
+        .requestHandler(router)
+        .listen(8080, ar -> {
+          if (ar.succeeded()) {
+            System.out.println("Server started on port 8080");
+            startPromise.complete();
+          } else {
+            System.out.println("Server start failed: " + ar.cause());
+            startPromise.fail(ar.cause());
+          }
+        });
+    }
   }
 }
