6f5df64eec4cc22df1384bfe66440112a7538acd
hendisantika <hendisantika@yahoo.co.id>
2025-01-09 07:39:24 +0700

    Add Main App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/weather_service/Main.java
@@ -0,0 +1,19 @@
+package id.my.hendisantika.weather_service;
+
+import io.vertx.core.AbstractVerticle;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : weather-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/01/25
+ * Time: 07.38
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+  public static class MyVerticle extends AbstractVerticle {
+
+  }
+}
