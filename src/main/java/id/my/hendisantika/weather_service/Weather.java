d6ad82a7a65fd3f6099e65f34a5faba3671228d1
hendisantika <hendisantika@yahoo.co.id>
2025-01-09 07:35:22 +0700

    Add ForecastItem Model🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/weather_service/Weather.java
+++ src/main/java/id/my/hendisantika/weather_service/Weather.java
@@ -87,4 +87,124 @@ public class Weather {
       this.items = items;
     }
   }
+
+  public static class ForecastItem {
+    private String date;
+    private String dateWithTimezone;
+    private double freshSnow;
+    private Object snowHeight;
+    private Weather weather;
+    private Precipitation prec;
+    private double sunHours;
+    private Object rainHours;
+    private Temperature temperature;
+    private Wind wind;
+    private Windchill windchill;
+    private SnowLine snowLine;
+    private Astronomy astronomy;
+
+    public String getDate() {
+      return date;
+    }
+
+    public void setDate(String date) {
+      this.date = date;
+    }
+
+    public String getDateWithTimezone() {
+      return dateWithTimezone;
+    }
+
+    public void setDateWithTimezone(String dateWithTimezone) {
+      this.dateWithTimezone = dateWithTimezone;
+    }
+
+    public double getFreshSnow() {
+      return freshSnow;
+    }
+
+    public void setFreshSnow(double freshSnow) {
+      this.freshSnow = freshSnow;
+    }
+
+    public Object getSnowHeight() {
+      return snowHeight;
+    }
+
+    public void setSnowHeight(Object snowHeight) {
+      this.snowHeight = snowHeight;
+    }
+
+    public Weather getWeather() {
+      return weather;
+    }
+
+    public void setWeather(Weather weather) {
+      this.weather = weather;
+    }
+
+    public Precipitation getPrec() {
+      return prec;
+    }
+
+    public void setPrec(Precipitation prec) {
+      this.prec = prec;
+    }
+
+    public double getSunHours() {
+      return sunHours;
+    }
+
+    public void setSunHours(double sunHours) {
+      this.sunHours = sunHours;
+    }
+
+    public Object getRainHours() {
+      return rainHours;
+    }
+
+    public void setRainHours(Object rainHours) {
+      this.rainHours = rainHours;
+    }
+
+    public Temperature getTemperature() {
+      return temperature;
+    }
+
+    public void setTemperature(Temperature temperature) {
+      this.temperature = temperature;
+    }
+
+    public Wind getWind() {
+      return wind;
+    }
+
+    public void setWind(Wind wind) {
+      this.wind = wind;
+    }
+
+    public Windchill getWindchill() {
+      return windchill;
+    }
+
+    public void setWindchill(Windchill windchill) {
+      this.windchill = windchill;
+    }
+
+    public SnowLine getSnowLine() {
+      return snowLine;
+    }
+
+    public void setSnowLine(SnowLine snowLine) {
+      this.snowLine = snowLine;
+    }
+
+    public Astronomy getAstronomy() {
+      return astronomy;
+    }
+
+    public void setAstronomy(Astronomy astronomy) {
+      this.astronomy = astronomy;
+    }
+  }
 }
