9b057a50d2226eaa44b18621fb61147c6dd78ae9
hendisantika <hendisantika@yahoo.co.id>
2025-01-09 07:33:39 +0700

    Add required libs🫸🌀✏️📗🐧🐳⬆

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,16 @@ dependencies {
   implementation("io.vertx:vertx-core")
   testImplementation("io.vertx:vertx-junit5")
   testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+
+//  testImplementation platform("org.junit:junit-bom:5.10.0")
+  testImplementation("org.junit.jupiter:junit-jupiter")
+  implementation("io.vertx:vertx-core:4.3.0")
+  implementation("io.vertx:vertx-web:4.3.0")
+  implementation("com.squareup.retrofit2:retrofit:2.9.0")
+  implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+  implementation("com.squareup.okhttp3:okhttp:4.9.3")
+  implementation("org.asynchttpclient:async-http-client:2.12.0")
+
 }
 
 java {
