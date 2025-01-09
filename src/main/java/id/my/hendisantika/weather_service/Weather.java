86e82abdf0dd0e86501dc2e848e0375269c03507
hendisantika <hendisantika@yahoo.co.id>
2025-01-09 07:34:27 +0700

    Add Coordinates Model🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/weather_service/Weather.java
+++ src/main/java/id/my/hendisantika/weather_service/Weather.java
@@ -50,4 +50,26 @@ public class Weather {
       this.coordinates = coordinates;
     }
   }
+
+  public static class Coordinates {
+    private double latitude;
+    private double longitude;
+
+    // Getters and setters
+    public double getLatitude() {
+      return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+      this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+      return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+      this.longitude = longitude;
+    }
+  }
 }
